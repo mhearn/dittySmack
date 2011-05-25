41f84a6af12eb7a886127750b5a7fcc4741a535b
Michael Hearn <michael.e.hearn@gmail.com>
2011-05-25 14:10:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DittySmack::Application.config.secret_token = 'ce22b3dea2f135d8b68aecf02694d1b3e7560e0fd74b422e27c3f58cb6c3f7751f1b15ed35a5b963d7c7fdf14c2faab4b4af47e878fafb23f28bfee13d37532d'
